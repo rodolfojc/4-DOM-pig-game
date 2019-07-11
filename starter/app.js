7a61d5138f7cffbcf5a332acf3c843a95d6dbbd1
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-07-11 22:29:02 +0100

    DRY - Don't repeat yourselt

--- starter/app.js
+++ starter/app.js
@@ -59,8 +59,39 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
         document.querySelector('#current-'+activePlayer).textContent = roundScore;
     }else {
         // NEXT PLAYER
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        roundScore = 0;
+        nextPlayer();
+    }
+    
+    
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    
+    // 1.- ADD CURRENT SCORE TO GLOBAL SCORE
+    scores[activePlayer] += roundScore;
+    
+    // 2.- UPDATE UI
+    document.querySelector('#score-'+activePlayer).textContent = scores[activePlayer];
+    
+    // 3.- CHECK IF PLAYER WON THE GAME
+    if (scores[activePlayer] >= 20){
+        document.querySelector('#name-'+activePlayer).textContent = 'Winner!';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    } else {
+        // 4.- NEXT PLAYER
+        nextPlayer();
+    }
+    
+    
+    
+})
+
+function nextPlayer(){
+    
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
         
         document.getElementById('current-0').textContent = 0;
         document.getElementById('current-1').textContent = 0;
@@ -73,12 +104,7 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
         //document.querySelector('.player-1-panel').classList.add('active');
         
         document.querySelector('.dice').style.display = 'none';
-    }
-    
-    
-});
-
-
+}
 
 
 
