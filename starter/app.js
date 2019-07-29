73b4ff13161a939512e0bfb14ee0b63657fd3794
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-07-29 22:49:35 +0100

    Update app.js

--- starter/app.js
+++ starter/app.js
@@ -112,6 +112,12 @@ function init() {
     document.getElementById('current-1').textContent = 0;
     document.getElementById('name-0').textContent = 'Player 1';
     document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+    
     
 
 }
